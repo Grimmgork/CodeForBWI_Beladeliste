c70c9344e402c86e826450dd9de43696c1c21a67
Eric <mmmru7shj@gmail.com>
2020-12-28 03:21:19 +0100

    Working on it!

--- Beladeliste/Program.cs
+++ Beladeliste/Program.cs
@@ -11,7 +11,7 @@ namespace Beladeliste
         static void Main(string[] args)
         {
             //data
-            Equipment[] data = new Equipment[10] {
+            Equipment[] context = new Equipment[10] {
                 new Equipment("Notebook Büro 13", 205, 2451, 40),
                 new Equipment("Notebook Büro 14", 420, 2978, 35),
                 new Equipment("Notebook outdoor", 450, 3625, 80),
@@ -24,34 +24,25 @@ namespace Beladeliste
                 new Equipment("Tablet outdoor groß", 370, 1980, 68)
             };
 
-            //mapping the name of each position to a index
+            //mapping the name of each position to a index (to sum up the value at the e)
             Dictionary<string, int> nameIndexMap = new Dictionary<string, int>();
-            for(int i = 0; i < data.Length;i++){
-                nameIndexMap.Add(data[i].name, i);
+            for(int i = 0; i < context.Length;i++){
+                nameIndexMap.Add(context[i].name, i);
             }
 
             //max available payload of each vehicle 
             int[] payloads = new int[2] { 941900, 941900 };
 
-            Console.WriteLine("All equipment for Bonn:");
-            Console.WriteLine("---------------------------------------------------");
-            foreach (Equipment e in data)
-            {
-                Console.WriteLine(e.name + "\t" + e.requiredQuantity + "\t" + e.weight + "\t" + e.value );
-            }
-            Console.WriteLine();
-            Console.WriteLine();
-
             //generate a loading-list for each vehicle
-            PacklistItem[][] packingLists = Distribute(data, payloads);
+            Dictionary<string, int>[] packingLists = Distribute(context, payloads);
 
             int valueCounter = 0;
             
             //visualize results
             for (int i = 0; i < packingLists.Length; i++)
             {
-                PacklistItem[] l = packingLists[i];
-                if (l == null)
+                Dictionary<string, int> packlist = packingLists[i];
+                if (packlist == null)
                 {
                     Console.WriteLine("List missing!");
                     continue;
@@ -60,12 +51,13 @@ namespace Beladeliste
                 Console.WriteLine("Packing list for transport-" + (i + 1) + ":");
                 Console.WriteLine("----------------------------------------");
 
-                int numberOfPositions = l.Length;
-                for(int p = 0; p < numberOfPositions; p++)
+                List<string> positionNames = packlist.Keys.ToList();
+                positionNames.Sort();
+                foreach (string positionName in positionNames)
                 {
-                    PacklistItem pos = l[p];
-                    valueCounter += data[nameIndexMap[pos.name]].value * pos.quantity;
-                    Console.WriteLine(pos.name + "\t" + pos.quantity + " Stk.");
+                    int quantity = packlist[positionName];
+                    valueCounter += context[nameIndexMap[positionName]].value * quantity;
+                    Console.WriteLine(positionName + "\t" + quantity + " Stk.");
                 }
 
                 Console.WriteLine();
@@ -81,58 +73,19 @@ namespace Beladeliste
         }
 
 
-        public static PacklistItem[][] Distribute(Equipment[] data, int[] payloads)
+        public static Dictionary<string,int>[] Distribute(Equipment[] context, Dictionary<string, int> packlist, int[] payloads)
         {
-            List<Equipment> sortedData = data.OrderByDescending(Equipment => Equipment.ValuePerWeightRatio).ThenBy(Equipment => Equipment.weight).ToList();
-            PacklistItem[][] result = new PacklistItem[payloads.Length][];
-            Dictionary<string, int> quantities = new Dictionary<string, int>();
+            List<Equipment> sorted = context.OrderByDescending(Equipment => Equipment.ValuePerWeightRatio).ThenBy(Equipment => Equipment.weight).ToList();
 
-            for (int i = 0; i < payloads.Length; i++)
-            {
-                int availableWeight = payloads[i];
-                List<PacklistItem> packList = new List<PacklistItem>();
-                
-                for(int eqIndex = 0; eqIndex < sortedData.Count; eqIndex++)
-                {
-                    Equipment eq = sortedData[eqIndex];
-                    if (i == 0){ //build the quantity-dictionary on the first traversal of all positions
-                        quantities.Add(eq.name, eq.requiredQuantity);
-                    }
 
-                    int maxQuantity = availableWeight / eq.weight;
-
-                    if (maxQuantity == 0 || quantities[eq.name] == 0)
-                        continue;
-
-                    int quantity = Math.Min(eq.requiredQuantity, maxQuantity);
-
-                    packList.Add(new PacklistItem(eq.name, quantity));
-                    availableWeight -= quantity * eq.weight;
-                    quantities[eq.name] -= quantity;
-                }
-
-                result[i] = packList.ToArray();
-            }
+            Dictionary<string, int>[] result = new Dictionary<string, int>[payloads.Length];
 
             return result;
-        }
-
-        public struct PacklistItem
-        {
-            public string name { get; private set; }
-            public int quantity { get; private set; }
-
-            public PacklistItem(string name, int quantity)
-            {
-                this.name = name;
-                this.quantity = quantity;
-            }
         }
 
         public struct Equipment
         {
             public string name { get; private set; }
-            public int requiredQuantity { get; private set; }
             public int weight { get; private set; }
             public int value { get; private set; }
 
@@ -144,10 +97,9 @@ namespace Beladeliste
                 }
             }
 
-            public Equipment(string name, int requiredQuantity, int weight, int value)
+            public Equipment(string name, int weight, int value)
             {
                 this.name = name;
-                this.requiredQuantity = requiredQuantity;
                 this.weight = weight;
                 this.value = value;
             }
